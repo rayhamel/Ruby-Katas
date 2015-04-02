5cbed772681d1d4b18d5b9e1dbe66a9bcb8a11cc
Ray Hamel <rayghamel@gmail.com>
2015-04-02 01:21:22 -0400

    further refactor caesar_cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,7 +3,7 @@ def caesar_cipher(str, number)
   number %= 26
   ciphered_string = ''
   str.each_codepoint do |pnt|
-    if pnt <= 90 && (pnt + number) > 90 or pnt <= 122 && (pnt + number) > 122
+    if pnt < 91 && (pnt + number) > 90 or pnt < 123 && (pnt + number) > 122
       ciphered_string.concat(pnt + number - 26)
     elsif pnt > 64 && pnt < 91 or pnt > 96 && pnt < 123
       ciphered_string.concat(pnt + number)
